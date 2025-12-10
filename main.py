33ff8648edcd4e936475a54831b65586d90c9e28
Rakesh Bhugra <81642485+rakeshbhugra@users.noreply.github.com>
2025-12-10 11:34:47 +0530

    added webhook router

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+class RequestData(BaseModel):
+    aqi_level: str
+    warning_message: str
+
+class ResponseData(BaseModel):
+    status: str
+    message: str
+
+
+@app.post("/webhook", response_model=ResponseData)
+async def process_webhook(data: RequestData):
+    print(f"Received AQI Level: {data.aqi_level}")
+    return ResponseData(status="success", message="Data processed successfully")
+
